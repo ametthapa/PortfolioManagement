2602396fd0c1d644001cfdf499f0b61013f1f132
Amrit Thapa <thapa.amet27@gmail.com>
2021-10-31 21:04:53 +0545

    this is

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,5 +1,19 @@
 const List = () => {
-  return <div>This is List</div>;
+  return (
+    <div className=" h-screen p-10">
+      <div className="font-bold text-2xl text-gray-700 pb-5">All Stocks</div>
+      <table className="border-collapse border border-gray-400 table-auto">
+        <tr>
+          <th className="border border-gray-400 p-3">S_N.</th>
+          <th className="border border-gray-400 p-3">Stock Name</th>
+          <th className="border border-gray-400 p-3">TransactionType</th>
+          <th className="border border-gray-400 p-3">Quantity</th>
+          <th className="border border-gray-400 p-3">Amount</th>
+          <th className="border border-gray-400 p-3">TransactionDate</th>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default List;
